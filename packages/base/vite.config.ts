5dc3ae80b8e78c99fb4d303e68f808d28c435b19
Vlad <2pi14b.donchenko@gmail.com>
2022-11-06 21:19:54 +0200

    chore(config): add lib config

--- packages/base/vite.config.ts
+++ packages/base/vite.config.ts
@@ -1,10 +1,26 @@
 /// <reference types="vitest" />
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    lib: {
+      name: 'base',
+      entry: resolve(__dirname, 'src/index.ts'),
+      fileName: 'base',
+    },
+    rollupOptions: {
+      external: ['vue'],
+      output: {
+        globals: {
+          vue: 'Vue',
+        },
+      },
+    },
+  },
   test: {
     globals: true,
     environment: 'happy-dom',
